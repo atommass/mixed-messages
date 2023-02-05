710d359632d2d5ffbf0f7fdfa07d0655798b65d4
atommass <elgars.ab@gmail.com>
2023-02-05 22:28:18 +0200

    Generates a random unfunny joke

--- mixed-messages.js
+++ mixed-messages.js
@@ -19,7 +19,7 @@ const randomNoNamer = () => {
     }
 }
 
-// 
+let randomThingNo;
 const generateRandomThing = randNoName => {
     randomThingNo = randomNumToFive();
     switch (randomThingNo) {
@@ -54,7 +54,61 @@ const generateRandomThing = randNoName => {
                 return `${randNoName} Programmers`;
             }
     }
+    
 } 
-for (let i = 0; i < 15; i++) {
-    console.log(generateRandomThing(randomNoNamer()));
+
+const firstLine = `${generateRandomThing(randomNoNamer())} walk into a bar,`;
+
+var thingCount;
+if (firstLine.includes('One')) {
+    thingCount = 1;
+} else if (firstLine.includes('Two')) {
+    thingCount = 2;
+} else if (firstLine.includes('Three')) {
+    thingCount = 3;
+} else if (firstLine.includes('Four')) {
+    thingCount = 4;
+} else if (firstLine.includes('Five')) {
+    thingCount = 5;
+} else {
+    thingCount = 0;
 }
+
+const responses = [
+    "It's hot in here.",
+    "Shut your mouth!",
+    "Bartender, how much do I owe you?",
+    "We'd like a couple of beers, please.",
+    "Is the bar tender here?",
+    "You read my mind, buddy.",
+    "I'm all ears!",
+    "Want to hear a joke?",
+    "Do you have any helicopter flavored potato chips?",
+    "Boy, I wish I could do that."
+];
+
+const sequenceNumber = ['first', 'second', 'third', 'fourth', 'fifth'];
+
+const whatBartenderSays = [
+    "Sorry, we don't cater for functions.",
+    "Hey. This is a singles bar.",
+    "We don't serve time travelers in here.",
+    "Okay, but don't start anything.",
+    "We don't serve your type in here.",
+    "Why the short face?",
+    "Have you been served?",
+    "How it's going?"
+];
+
+console.log(firstLine);
+console.log();
+for (let i = 0; i < thingCount; i++) {
+    if (thingCount > 1) {
+        console.log(`The ${sequenceNumber[i]} says: "${responses[Math.floor(Math.random() * responses.length)]}"`);
+    } else {
+        console.log(`And says: "${responses[Math.floor(Math.random() * responses.length)]}"`);
+    }
+}
+console.log();
+console.log(`The bartender says: "${whatBartenderSays[Math.floor(Math.random() * whatBartenderSays.length)]}"`);
+// console.log(thingCount);
