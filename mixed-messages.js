49e2689949292642eab892de6050e151704336c1
atommass <elgars.ab@gmail.com>
2023-02-05 15:50:19 +0200

    Generates and names random number

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,2 +1,27 @@
 console.log("Here's your most unfunny joke of the day!");
 
+// FIRST JOKE OPTION!
+// 1st random number generator
+const randomNumToFive = () => {
+    const randToFive = Math.floor((Math.random() * 5) + 1);
+    return randToFive;
+}
+
+// returns the name of random number
+const randomNoNamer = () => {
+    randomThingNo = randomNumToFive();
+    switch (randomThingNo) {
+        case 1: 
+            return 'One';
+        case 2: 
+            return 'Two';
+        case 3: 
+            return 'Three';
+        case 4: 
+            return 'Four';
+        case 5: 
+            return 'Five';
+    }
+}
+
+console.log(randomNoNamer());
